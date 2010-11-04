fda5080daff57f8497e2d4811aecfc844b94f5f7
Nick Crawford <nick@squamate.local>
2010-11-03 22:28:26 -0400

    Updated seqcap.py with 'aligns2oneliners' function that converts phylip to a single comma delimited line
    Parsing is fun (sometimes)

--- temp/seqcap.py
+++ temp/seqcap.py
@@ -23,7 +23,7 @@ The author may be contacted at ngcrawford@gmail.com
 """
 
 import os, sys, glob, shlex, subprocess
-def mapper(args):
+def fasttree(args):
     """sends the individual """
     for line in sys.stdin:
         path = line.strip()
@@ -31,15 +31,84 @@ def mapper(args):
         # pathname = os.path.abspath(pathname)
         # print pathname
         # this will probably need to be changed for aws
-        command = './FastTree -nt -quiet %s' % (path)
-        print command
+        command = './PhyML -nt -quiet %s' % (path)
         command = shlex.split(command)
+        print subprocess.Popen(command, stderr=subprocess.STDOUT, \
+                        stdout=subprocess.PIPE).communicate()# [0]
+        
 
-            
+def phyml(args):
+    for line in sys.stdin:
+        path = line.strip()
+        
+def muscle(args):
+    
+    # [('seq1','aaatttc'),('seq2','ggaaaatc')] to be aligned, and
+    # aln_from_stdout will be a string containing the fasta alignment, ripe
+    # for the parsing.
+    # 
+    # Note that as of muscle 3.8 -stable is no longer available (stupid Bob
+    # Edgar independent scientist :) so you'll need to drop the
+    # ever-so-handy -stable flag if you're using a muscle after 3.7
+    # 
+    # mh = Popen(['muscle','-stable'],stdin=PIPE,stderr=PIPE,stdout=PIPE)
+    # mh.stdin.write('\n'.join(['>%s\n%s' % (l,s) for l,s in label_seqs]))
+    # aln_from_stdout,output_from_stderr = mh.communicate()
+    
+    seqs = sys.argv[1].split(',')
+    label_seqs = zip(seqs[:-1:2],seqs[1::2])
+    
+    mh = Popen(['muscle','-stable'],stdin=PIPE,stderr=PIPE,stdout=PIPE)
+    mh.stdin.write('\n'.join(['>%s\n%s' % (l,s) for l,s in label_seqs]))
+    alnstr = mh.communicate()[0]
+    print ','.join(alnstr.strip().replace('>','').split('\n'))
+
+def aligns2oneliners(args):
+
+    # Storage Dictionaries
+    taxa_id_dict = {}
+    taxa_seq_dict = {}
+    
+    for count, line in enumerate(sys.stdin):    
+        
+        # Process taxa count and alignment length line
+        if count == 0:
+            taxa_count, align_len = line.strip().split(' ')
+            taxa_count = int(taxa_count.strip()) 
+            align_len = int(align_len.strip())
+        
+        count = count -1    # update count to skip header
+        
+        if len(line.strip()) == 0: continue # skip blank lines
+        
+        # Do initial dictionary data population
+        if 0 <= count <= taxa_count:
+            taxa_name = line[:11].strip()
+            sequence = line[10:].strip()
+            taxa_id_dict[count] = taxa_name
+            taxa_seq_dict[taxa_name] = sequence.replace(' ','')
+        
+        # add extra seqeunces to taxa/seq dictionary
+        if count > taxa_count:
+            dict_id = count % tcount_plus_extra
+            taxa_name = taxa_id_dict[dict_id]
+            taxa_seq_dict[taxa_name] = taxa_seq_dict[taxa_name] + line.replace(' ','').strip()
+        
+        tcount_plus_extra = taxa_count + 1
+    
+    final_line = ""
+    for key, value in taxa_seq_dict.iteritems():
+        final_line += '%s, %s, ' % (key, value)
+    final_line = final_line[:-2]
+    final_line + "\n"
+    print final_line
+
+                
 def main():
-    if sys.argv[1] == "mapper":
-      mapper(sys.argv[2:])
-
+    if sys.argv[1] == "fasttree":
+        mapper(sys.argv[2:])
+    if sys.argv[1] == 'convert-file':
+        aligns2oneliners(sys.argv[2:])
 
 if __name__ == '__main__':
     main()
