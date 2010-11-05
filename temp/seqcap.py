b1034b220a765aa6020cfe94cb1c1481bfb2ee18
Nick Crawford <nick@wireless1x-13-185.bu.edu>
2010-11-05 15:12:44 -0400

    Updates to seqcap.py

--- temp/seqcap.py
+++ temp/seqcap.py
@@ -25,9 +25,13 @@ The author may be contacted at ngcrawford@gmail.com
 import os
 import sys 
 import glob
+import shlex
+import tempfile
+import random
+from tree import Tree
 from subprocess import *
 
-def oneliners2phylip(line):
+def oneliner2phylip(line):
     seqs = line.split(',')
     label_seqs = zip(seqs[:-1:2],seqs[1::2])
     taxa_count = len(label_seqs)
@@ -38,6 +42,44 @@ def oneliners2phylip(line):
         alignment += '%-*s%s\n' % (10, taxa_name, seq)
     return alignment
 
+
+def phyml(args):
+    """sends the individual
+    cat practice_alignments/3.align.oneliners.txt | ./seqcap.py fasttree
+    """
+    
+    for line in sys.stdin:
+        phylip = oneliner2phylip(line) # convert line to phylip
+        
+        # SETUP TEMP FILE
+        temp_in = tempfile.NamedTemporaryFile(suffix='.out',dir='tmp/') #c c
+        for line in phylip:
+            temp_in.write(line)
+        temp_in.seek(0)     # move pointer to beginning of file
+        
+        # RUN PHYML
+        cli = './PhyML_3.0 --input {0}'.format(temp_in.name)
+        cli_parts = shlex.split(cli)
+        ft = Popen(cli_parts, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+        ft.communicate()[0]
+        
+        # EXTRACT RESULTS
+        temp_string = os.path.split(temp_in.name)[1].split('.')[0]
+        treefile =  'tmp/%s.out_phyml_tree.txt' % temp_string
+        newick = open(treefile,'r').readlines()[0].strip()
+        
+        # ROOT TREE
+        tree = Tree(newick)
+        tree.set_outgroup('anoCar2')
+        newick = tree.write(format=5)
+        print newick.strip()
+        
+        # CLEAN UP TEMPFILES
+        temp_in.close()
+        for filename in glob.glob('tmp/%s.*' % (temp_string)) :
+            os.remove( filename )
+        
+
 def fasttree(args):
     """sends the individual
     cat practice_alignments/3.align.oneliners.txt | ./seqcap.py fasttree
@@ -45,9 +87,14 @@ def fasttree(args):
     for line in sys.stdin:
         ft = Popen(['./FastTree','-nt','-quiet'],stdin=PIPE,stderr=PIPE,stdout=PIPE)
         ft.stdin.write(oneliners2phylip(line))
-        alnstr = ft.communicate()[0]
-        print alnstr.strip()
-        
+        tree_str = ft.communicate()[0]
+        tree_str = tree_str.strip()
+        print tree_str
+        tree = Tree(tree_str)
+        tree.set_outgroup('anoCar2')
+        newick = tree.write(format=5)
+        print newick.strip()
+
 def muscle(args):
     
     # [('seq1','aaatttc'),('seq2','ggaaaatc')] to be aligned, and
@@ -71,7 +118,7 @@ def muscle(args):
 def aligns2oneliners(args):
     """This code converts phylip alignments to a single tab-delimited line
         of taxa ids and sequence pairs"""
-    
+        
     # Storage Dictionaries
     taxa_id_dict = {}
     taxa_seq_dict = {}
@@ -114,22 +161,67 @@ def aligns2oneliners(args):
         final_line + "\n"
         print final_line
 
-def mpest(args):
-    for line in sys.stdin:
-        print line
-        print 'awesome'
 
-def fastas2oneliners(args):
-    pass
+
+
+def mpest(args):
+    
+    def create_control_files(path):
+        """explaination"""
+        base_path, tree_file = os.path.split(path)
+
+        control_file = '.'.join([tree_file.split('.')[0],'control'])
+        fout = open(os.path.join(base_path, control_file),'w')
+        # open up the input tree file and get the first tree
+        tree_file_contents = open(path,'r').readlines()
+        tree = tree_file_contents[0].strip()
+        tree = Tree(tree)
+        taxa_names =  tree.get_leaf_names()
+        #pdb.set_trace()
+        taxa_string  = ['{0}\t1\t{0}'.format(taxa) for taxa in taxa_names]
+        template_info = {'filename':path,
+                        'tree_count':len(tree_file_contents),
+                        'numb_taxa':len(taxa_names),
+                        'taxa_details':'\n'.join(taxa_string)}
+        # needs to go to format()
+        template = "%(filename)s\n0\n%(tree_count)s %(numb_taxa)s\n%(taxa_details)s\n0\n" % template_info
+        fout.write(template)
+        fout.close()
+    
+    def execute_mpest():
+        seed = random.randint(0,1000000)
+        cli = "./mpest-bf-v1 rooted.control %s mpest.species.tree" % (seed)
+        cli_parts = shlex.split(cli)
+        mp = Popen(cli_parts, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+        mp.communicate()[0]
+        
+        for line in open('mpest.species.tree'):
+            print line
+    
+    fout = open('rooted.trees','w')
+    for line in sys.stdin:
+        fout.write(line)
+    fout.close()
+    
+    create_control_files('rooted.trees')
+    execute_mpest()
+    
+    # CLEAN UP TREE/STAT FILES
+    for filename in glob.glob('rooted.*') :
+        os.remove( filename )
+    
     
 def main():
     
-    if sys.argv[1] == "fasttree":
-        fasttree(sys.argv[2:])
+    # if sys.argv[1] == "fasttree":
+    #     fasttree(sys.argv[2:])
     
     if sys.argv[1] == "mpest":
         mpest(sys.argv[2:])
 
+    if sys.argv[1] == "phyml":
+        phyml(sys.argv[2:])
+            
 if __name__ == '__main__':
     main()
 
