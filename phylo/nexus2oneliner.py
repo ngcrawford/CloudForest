1d77bba74e7432671a922f7b6dff54a1e5376400
Nick Crawford <ngcrawford@gmail.com>
2011-06-29 20:13:28 -0400

    nexux2...py now deals taxa lacking underscores

--- phylo/nexus2oneliner.py
+++ phylo/nexus2oneliner.py
@@ -13,21 +13,25 @@ def nexus2oneliner():
         
         if "#NEXUS" in line:
             in_data = False
-            if as_one_liner == True:
+            if as_one_liner == True and len(final_line) != 0:
                 sys.stdout.write(final_line + ";")
-            else:
+            if as_one_liner == False and len(final_line) != 0:
                 sys.stdout.write(final_line + "\n")
             final_line = ''
             align_count += 1
         
         if in_data == True:
             if ';' not in line:
+                
                 taxon, seq = line.strip().split()
-                genus, species = taxon.split('_')[-2:]
-                genus_trunc = '%s%s' % (genus[0].upper(), genus[1:3])
-                species_cap =  '%s%s' % (species[0].upper(), species[1:])
-                taxon = genus_trunc + species_cap
-                taxon =  taxon[:8]
+                if '_' in taxon:
+                    genus, species = taxon.split('_')[-2:]
+                    genus_trunc = '%s%s' % (genus[0].upper(), genus[1:3])
+                    species_cap =  '%s%s' % (species[0].upper(), species[1:])
+                    taxon = genus_trunc + species_cap
+                    taxon =  taxon[:8]
+                else:
+                    taxon = taxon[0].upper() + taxon[1:8]
                 if final_line == '':
                     if as_one_liner == True:
                         final_line = taxon +','+ seq
