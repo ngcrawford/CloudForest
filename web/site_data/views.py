93648da0330813920e335d68b9ef007862a96368
Nick Crawford <ngcrawford@gmail.com>
2011-10-21 14:15:57 -0400

    Added an S3 url validator.

--- web/site_data/views.py
+++ web/site_data/views.py
@@ -1,6 +1,6 @@
 from site_data import app
 from flask import Flask, request, flash, redirect, url_for, render_template
-from flaskext.wtf import Form, TextField, Required, BooleanField, SelectField,  RadioField, SubmitField, IntegerField, validators
+from flaskext.wtf import Form, TextField, Required, BooleanField, SelectField,  RadioField, SubmitField, IntegerField, validators, ValidationError
 
 # app = Flask(__name__)
 
@@ -10,14 +10,29 @@ app.config.update(
 )
 
 class MyForm(Form):
+
+	def validate_S3_url(form, field):
+		"""Checks that field is a valid S3 url.
+			Needs improvement. :)
+		"""
+		if field.data == "s3n://":
+			raise ValidationError(u'This field must be an S3 url.')
+
 	aws_id = TextField('AWS ID', validators=[Required()])
 	aws_secret_key = TextField('AWS Secret Key', validators=[Required()])
 	aws_keypair_name = TextField('AWS Keypair Name', validators=[Required()])
-	input_url = TextField('Input Url', validators=[Required()])
-	output_url = TextField('Output Url', validators=[Required()])
-	job_name = TextField('Job Name', default="CloudForest")
+	input_url = TextField('Input Url', validators=[Required(), validate_S3_url])
+	output_url = TextField('Output Url', validators=[Required(), validate_S3_url])
+	job_name = TextField('Job Name', default="CloudForest", validators=[Required()])
 	job_type = SelectField('Job Type', choices=[('genetrees', 'Gene Trees'), ('bootstraps','Bootstraps')])
 	mraic = BooleanField('Use MrAIC to infer models',default=True)
+	map_tasks = IntegerField('Map Tasks', default=19)
+	reduce_tasks = IntegerField('Reduce Tasks', default=1)
+	bootstraps = IntegerField('Bootstrap Replicates', default=0)
+	
+
+	
+	
 	
 # if os.sys.platform == 'darwin':
 #   result_url = "/query_args"
