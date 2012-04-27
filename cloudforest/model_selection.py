80446c440fa2dce1e004e6c001c54593adbaea9f
Brant Faircloth <brant@faircloth-lab.org>
2012-04-27 11:09:48 -0700

    fully working aicc model selection in Phyml class

--- cloudforest/model_selection.py
+++ cloudforest/model_selection.py
@@ -53,6 +53,32 @@ class Phyml:
                 'GTRG':"+\nM\nM\nM\nF\nY\n",
                 'GTRIG':"+\nM\nM\nM\nF\nV\nY\nY\n"
             }
+        self.numparams = {
+                'JC69':0,
+                'JC69I':1,
+                'JC69G':1,
+                'JC69IG':2,
+                'F81':3,
+                'F81I':4,
+                'F81G':4,
+                'F81IG':5,
+                'K2P':1,
+                'K2PI':2,
+                'K2PG':2,
+                'K2PIG':3,
+                'HKY':4,
+                'HKYI':5,
+                'HKYG':5,
+                'HKYIG':6,
+                'SYM':5,
+                'SYMI':6,
+                'SYMG':6,
+                'SYMIG':7,
+                'GTR':8,
+                'GTRI':9,
+                'GTRG':9,
+                'GTRIG':10
+            }
 
     def _get_phyml_pth(self, pth, exe):
         if not exe:
@@ -67,13 +93,13 @@ class Phyml:
             phyml3 = os.path.join(pth, exe)
         return phyml3
 
-    def run(self):
+    def run(self, phylip, model='GTR'):
         pass
 
-    def _get_log_like(self, regex, phylip):
+    def _get_log_like(self, statfile, regex, phylip):
         result = None
-        stats = ''.join([phylip, '_phyml_stats.txt'])
-        for line in open(stats, 'rU'):
+        #stats = ''.join([phylip, '_phyml_stats.txt'])
+        for line in open(statfile, 'rU'):
             result = regex.search(line)
             if result:
                 break
@@ -81,46 +107,65 @@ class Phyml:
             raise ValueError("No Log-likelihood found")
         return float(result.groups()[0])
 
-    def _get_aic_tree(self, phylip):
+    def _get_aic_tree(self, treefile):
         tree = None
-        treefile = ''.join([phylip, '_phyml_tree.txt'])
+        #treefile = ''.join([phylip, '_phyml_tree.txt'])
         tree = open(treefile, 'rU').read().strip()
         if tree is None or tree == '':
             raise ValueError("No tree found")
         return tree
 
+    def _compute_aicc(self, model, loglik, count_branches=True):
+        """AICc is -2lnL + 2K + 2K(K+1)/n-K-1"""
+        if count_branches:
+            params = self.numparams[model] + self.nbranch
+        else:
+            params = self.numparams[model]
+        return -2. * loglik + 2. * params + ((2. * params * (params + 1.)) / (self.nchar - params - 1.))
+
+    def _get_taxon_and_char_data(self, regex):
+        # get taxon and character data for file
+        first_line = open(self.phylip, 'rU').readline()
+        self.taxa, self.nchar = [int(val) for val in regex.search(first_line).groups()]
+        self.nbranch = (2 * self.taxa) - 3
+        self.max_params = self.nbranch + 10
+
     def select_model(self):
         # compile this once
-        regex = re.compile("Log-likelihood:\s+(.+)")
+        ll_regex = re.compile("Log-likelihood:\s+(.+)")
+        dim_regex = re.compile("\s*(\d+)\s+(\d+)")
         results = {}
         # get current dir
         cwd = os.getcwd()
+        # create tempdir to hold phyml output on
+        # per locus level
+        working = tempfile.mkdtemp(dir='tmp')
+        os.chdir(working)
+        # copy phylip to working - phyml does like long paths
+        # copy file to tempdir once - work in tempdir - delete temp
+        # out files after computing aicc.
+        shutil.copyfile(self.phylip, os.path.basename(self.phylip))
+        self._get_taxon_and_char_data(dim_regex)
         for model, template in self.models.iteritems():
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            # create tempfile to hold phyml commands
-            working = tempfile.mkdtemp(dir='tmp')
             phylip = os.path.basename(self.phylip)
             template = "%s\n%s" % (phylip, template)
-            #pdb.set_trace()
-            # copy phylip to working
-            shutil.copyfile(
-                self.phylip,
-                os.path.join(working, os.path.basename(self.phylip))
-                )
-            os.chdir(working)
             cli = [self.phyml3]
             subprocess.Popen(
                     cli,
                     stdin=subprocess.PIPE,
                     stdout=subprocess.PIPE
                 ).communicate(input=template)
-            loglik = self._get_log_like(regex, phylip)
-            tree = self._get_aic_tree(phylip)
-            results[model] = [loglik, tree]
-            os.chdir(cwd)
-            shutil.rmtree(working)
-        pdb.set_trace()
+            statfile, treefile = [''.join([phylip, ext]) for ext in ['_phyml_stats.txt', '_phyml_tree.txt']]
+            lnl = self._get_log_like(statfile, ll_regex, phylip)
+            aicc = self._compute_aicc(model, lnl)
+            tree = self._get_aic_tree(treefile)
+            results[aicc] = [model, tree]
+            [os.remove(f) for f in [statfile, treefile]]
+        os.chdir(cwd)
+        shutil.rmtree(working)
+        # best is min(AICc)
+        best = min(results.keys())
+        return results[best][0], results[best][1]
 
 
 def main():
