527b534219a8a760d454ac7bebc49ba631fd8829
Brant Faircloth <brant.faircloth@gmail.com>
2012-05-12 10:16:31 -0700

    slightly reformat cloudforest_emr tests

--- cloudforest/tests/test_cloudforest_emr.py
+++ cloudforest/tests/test_cloudforest_emr.py
@@ -19,13 +19,16 @@ class TestCloudForestFunctions(unittest.TestCase):
         """Tests that creating genetrees with Cloudforest
             makes the appropriate number of trees.
         """
-        
-        test_data = open('alignments/3.oneliners','rU')
-        mr_job = ProcessPhyloData(['-r', 'local', '--setup-cmd', 
-                                    'mkdir -p tmp','--gene-trees',
-                                    '--archive=../gzips/osx.phylo.tar.gz#bin',
-                                    "-"])
-    
+
+        test_data = open('alignments/3.oneliners', 'rU')
+        mr_job = ProcessPhyloData([
+                '-r', 'local',
+                '--setup-cmd',
+                'mkdir -p tmp',
+                '--gene-trees',
+                '--archive=../gzips/osx.phylo.tar.gz#bin',
+                "-"]
+            )
         mr_job.sandbox(stdin=test_data)
         results = []
         with mr_job.make_runner() as runner:
@@ -33,9 +36,8 @@ class TestCloudForestFunctions(unittest.TestCase):
             for line in runner.stream_output():
                 # Use the job's specified protocol to read the output
                 key, value = mr_job.parse_output_line(line)
-                results.append(value) 
-        
-        self.assertEqual(len(results),3)
+                results.append(value)
+        self.assertEqual(len(results), 3)
 
     
     def test_MrAIC(self):
